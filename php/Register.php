e4dbbecb8906c73c2f50495d4a308c780f4314e4
dihungyu <egroup.intern.dihungyu@gmail.com>
2023-03-05 20:28:30 +0800

    feat: 註冊登入功能測試正常

--- php/Register.php
+++ php/Register.php
@@ -1,23 +1,37 @@
 <?php
-    session_start();
+session_start();
+
+//確認前端表單傳送不是空值
+if (isset($_POST["accountName"]) && isset($_POST["accountEmail"]) && isset($_POST["accountPhoneNumber"]) && isset($_POST["accountPassword"])) {
+
+    //變數設定
+    $accountName = $_POST["accountName"];
+    $accountEmail = $_POST["accountEmail"];
+    $accountPhoneNumber = $_POST["accountPhoneNumber"];
+    $accountPassword = $_POST["accountPassword"];
+
+    //加密密碼
+    $hashedPassword = password_hash($accountPassword, PASSWORD_DEFAULT);
+
+    //引入資料庫設定
     require_once("conn.php");
 
-    if(isset($_POST["accountName"]) && isset($_POST["accountEmail"]) && isset($_POST["accountPhoneNumber"]) && isset($_POST["accountPassword"])){
-        $accountName=$_POST["accountName"];
-        $accountEmail=$_POST["accountEmail"];
-        $accountPhoneNumber=$_POST["accountPhoneNumber"];
-        $accountPassword=$_POST["accountPassword"];
+    //檢查資料庫內是否已有帳戶
+    $stmt = $conn->prepare("SELECT accountEmail FROM accounts WHERE accountEmail = ?");
+    $stmt->bind_param("s", $accountEmail);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-        $result = $conn->query("select accountEmail from `accounts` where accountEmail='$accountEmail'");
-
-    if(mysqli_num_rows($result) >= 1){
-    
-    echo "<script>{window.alert('此信箱已被註冊！'); location.href='register.php'}</script>";
-    }
-     else{
-        $result2 = $conn->query("insert into accounts(accountName,accountEmail,accountPhoneNumber,accountPassword,level) values ('$accountName','$accountEmail','$accountPhoneNumber','$accountPassword','user')");
-        if($result2){
-            echo "<script>{window.alert('註冊成功！'); location.href='login.php'}</script>";
+    if (mysqli_num_rows($result) >= 1) {
+        echo "<script>{window.alert('此信箱已被註冊！'); location.href='../Register.html'}</script>";
+    } else {
+        $stmt = $conn->prepare("INSERT INTO accounts(accountId, accountName, accountPassword, accountEmail, accountPhoneNumber) VALUES ( UUID(), ?,?,?,?)");
+        $stmt->bind_param("ssss", $accountName, $hashedPassword, $accountEmail, $accountPhoneNumber);
+        if ($stmt->execute()) {
+            echo "<script>{window.alert('註冊成功！'); location.href='../LoginPage.html'}</script>";
+        } else {
+            echo "Error: " . $stmt->error;
         }
-     }
+    }
 }
+?>
