0b274901e7b914b641e381a8aac88262e230f37e
ryanbohongcho <egroup.intern.ryancho@gmail.com>
2023-03-11 22:56:33 +0800

    新增uuid_generator與上傳圖片功能

--- php/createCampsite.php
+++ php/createCampsite.php
@@ -10,7 +10,7 @@
 請輸入縣市編號：<input type="text" name="cityId" id="cityId"><br/>
 請輸入營區名稱：<input type="text" name="campsiteName" id="campsiteName"><br/>
 請輸入營區地址：<input type="text" name="campsiteAddress" id="campsiteAddress"><br/>
-請上傳營區圖片：<input type="file" name="file" id="file"><br/>
+請上傳營區圖片：<input type="file" name="files[]" multiple><br/>
 <input type="hidden" name="action" value="insert">
 <input type="submit" name="button" value="新增資料">
 <input type="reset" name="button2" value="重新填寫">
@@ -19,6 +19,8 @@
 </html>
 
 <?php
+
+
 if ($_SERVER["REQUEST_METHOD"] === "POST" && $_POST["action"] === "insert") {
 
     require_once("conn.php");
@@ -26,46 +28,59 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && $_POST["action"] === "insert") {
     $cityId = $_POST["cityId"];
     $campsiteName = $_POST["campsiteName"];
     $campsiteAddress = $_POST["campsiteAddress"];
+    require_once 'uuid_generator.php';
+    $campsiteId = uuid_generator();
 
-    if (isset($_FILES["file"]) && $_FILES["file"]["error"] === UPLOAD_ERR_OK) {
-        $allowed_types = [IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF];
-        $detected_type = exif_imagetype($_FILES["file"]["tmp_name"]);
+    $sql_query1 = "INSERT INTO campsites (campsiteId, cityId, campsiteName, campsiteAddress) 
+                VALUES ('$campsiteId', '$cityId', '$campsiteName', '$campsiteAddress')";
+                
+    mysqli_query($conn, $sql_query1);
+    
 
-        if (in_array($detected_type, $allowed_types)) {
-            $upload_dir = "/Applications/XAMPP/xamppfiles/htdocs/upload/";
-            $fileName = $_FILES["file"]["name"];
-            $filePath = $upload_dir . $fileName;
-            $fileExtensionName = pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION);
-            $fileSize = round($_FILES["file"]["size"] / 1024, 2); //KB
+    // loop through all uploaded files
+    foreach ($_FILES["files"]["name"] as $key => $name) {
 
-            move_uploaded_file($_FILES["file"]["tmp_name"], $filePath);
-
-            $sql_query1 = "INSERT INTO campsites (campsiteId, cityId, campsiteName, campsiteAddress) 
-                          VALUES (REPLACE(uuid(),'-',''), '$cityId', '$campsiteName', '$campsiteAddress')";
-
-            $sql_query2 = "INSERT INTO files (fileId, fileName, fileExtensionName, filePath, fileSize, fileCreateDate, filePathType) 
-            VALUES (REPLACE(uuid(),'-',''), '$fileName', '$fileExtensionName', '$filePath', $fileSize, now(), 'campsite')";
-
-            mysqli_query($conn, $sql_query1);
-            mysqli_query($conn, $sql_query2);
-
-            header("Location: readCampsite.php");
-            exit();
-        } else {
-            echo "上傳的檔案必須為圖片格式！";
+        // check if file was uploaded successfully
+        if ($_FILES["files"]["error"][$key] === UPLOAD_ERR_OK) {
+    
+            // check if file is an image
+            $allowed_types = [IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF];
+            $detected_type = exif_imagetype($_FILES["files"]["tmp_name"][$key]);
+    
+            if (in_array($detected_type, $allowed_types)) {
+                $fileId = uuid_generator();
+                $upload_dir = "/Applications/XAMPP/xamppfiles/htdocs/upload/";
+                $fileName = $_FILES["files"]["name"][$key];
+                $filePath = $upload_dir . $fileName;
+                $fileExtensionName = pathinfo($_FILES["files"]["name"][$key], PATHINFO_EXTENSION);
+                $fileSize = round($_FILES["files"]["size"][$key] / 1024, 2); //KB
+    
+                move_uploaded_file($_FILES["files"]["tmp_name"][$key], $filePath);
+    
+                $sql_query2 = "INSERT INTO files (fileId, campsiteId, fileName, fileExtensionName, filePath, fileSize, fileCreateDate, filePathType) 
+                VALUES ('$fileId', '$campsiteId', '$fileName', '$fileExtensionName', '$filePath', $fileSize, now(), 'campsite')";
+    
+                mysqli_query($conn, $sql_query2);
+    
+            } else {
+                echo "檔案 $name 必須為圖片格式！<br>";
+            }
+        } else if ($_FILES["files"]["error"][$key] !== UPLOAD_ERR_NO_FILE) {
+            $error_messages = [
+                UPLOAD_ERR_INI_SIZE => "檔案大小超出 php.ini:upload_max_filesize 限制",
+                UPLOAD_ERR_FORM_SIZE => "檔案大小超出 MAX_FILE_SIZE 限制",
+                UPLOAD_ERR_PARTIAL => "檔案大小僅被部份上傳",
+                UPLOAD_ERR_NO_TMP_DIR => "找不到暫存資料夾",
+                UPLOAD_ERR_CANT_WRITE => "檔案寫入失敗",
+                UPLOAD_ERR_EXTENSION => "上傳檔案被中斷",
+            ];
+            $error_code = $_FILES["files"]["error"][$key];
+    
+            echo "檔案 $name 上傳失敗：" . $error_messages[$error_code] . "<br>";
         }
-    } else if (isset($_FILES["file"]) && $_FILES["file"]["error"] !== UPLOAD_ERR_NO_FILE) {
-        $error_messages = [
-            UPLOAD_ERR_INI_SIZE => "檔案大小超出 php.ini:upload_max_filesize 限制",
-            UPLOAD_ERR_FORM_SIZE => "檔案大小超出 MAX_FILE_SIZE 限制",
-            UPLOAD_ERR_PARTIAL => "檔案大小僅被部份上傳",
-            UPLOAD_ERR_NO_TMP_DIR => "找不到暫存資料夾",
-            UPLOAD_ERR_CANT_WRITE => "檔案寫入失敗",
-            UPLOAD_ERR_EXTENSION => "上傳檔案被中斷",
-        ];
-        $error_code = $_FILES["file"]["error"];
-
-        echo $error_messages[$error_code];
     }
+
+    header("Location: readCampsite.php");
+    exit();
 }
 ?>
--- php/deleteCampsite.php
+++ php/deleteCampsite.php
@@ -3,9 +3,13 @@
 
     include('conn.php');
 
-    $sql_query = "DELETE FROM campsites WHERE campsiteId = '$id'";
+    $sql_query1 = "DELETE FROM campsites WHERE campsiteId = '$id'";
 
-    mysqli_query($conn,$sql_query);
+    mysqli_query($conn,$sql_query1);
+
+    $sql_query2 = "DELETE FROM files WHERE campsiteId = '$id'";
+
+    mysqli_query($conn,$sql_query2);
 
     header("Location: readCampsite.php");
 ?>
