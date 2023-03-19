656325c3950000658b57d9514ee01152a08245db
dihungyu <egroup.intern.dihungyu@gmail.com>
2023-03-19 16:26:12 +0800

    修復登入註冊功能

--- register.php
+++ register.php
@@ -1,83 +1,41 @@
 <?php
 session_start();
 
-if($_POST["FormType"]=="Login"){
-	// 確認使用者提交的表單中，是否填寫了帳號和密碼欄位
-	if (isset($_POST["accountEmail"], $_POST["accountPassword"])) {
+if ($_POST["FormType"] == "Register") {
+	//確認前端表單傳送不是空值
+	if (isset($_POST["accountName"]) && isset($_POST["accountGender"]) && isset($_POST["accountBirthday"]) && isset($_POST["accountEmail"]) && isset($_POST["accountPhoneNumber"]) && isset($_POST["accountPassword"])) {
 
-    	// 從 POST 取得填寫的帳號和密碼
-    	$accountEmail = $_POST["accountEmail"];
-    	$accountPassword = $_POST["accountPassword"];
+		//變數設定
+		$accountName = $_POST["accountName"];
+		$accountGender = $_POST["accountGender"];
+		$accountBirthday = $_POST["accountBirthday"];
+		$accountEmail = $_POST["accountEmail"];
+		$accountPhoneNumber = $_POST["accountPhoneNumber"];
+		$accountPassword = $_POST["accountPassword"];
+
+		//加密密碼
+		$hashedPassword = password_hash($accountPassword, PASSWORD_DEFAULT);
 
 		//引入資料庫設定
 		require_once("php/conn.php");
 
-    	// 搜尋資料庫中是否有符合帳號的資料
-    	$stmt = $conn->prepare("SELECT * FROM accounts WHERE accountEmail = ?");
-    	$stmt->bind_param("s", $accountEmail);
-    	$stmt->execute();
-    	$result = $stmt->get_result();
+		//檢查資料庫內是否已有帳戶
+		$stmt = $conn->prepare("SELECT accountEmail FROM accounts WHERE accountEmail = ?");
+		$stmt->bind_param("s", $accountEmail);
+		$stmt->execute();
+		$result = $stmt->get_result();
 
-    	// 若有符合的資料，則比對密碼是否正確
-    	if ($row = $result->fetch_assoc()) {
-        	if (password_verify($accountPassword, $row["accountPassword"])) {
-
-            	// 將使用者資料存入 session，並轉址至首頁
-            	$_SESSION["accountName"] = $row["accountName"];
-            	$_SESSION["accountEmail"] = $row["accountEmail"];
-            	$_SESSION["accountPhoneNumber"] = $row["accountPhoneNumber"];
-            	$_SESSION["accountPassword"] = $row["accountPassword"];
-            	$_SESSION["accountLevel"] = $row["accountLevel"];
-            	header("location:../index.html");
-        	} 
-			else {
-				// 若密碼不正確，則顯示錯誤訊息，並轉址至登入頁面
-            	echo "<script>{window.alert('密碼錯誤！請再試一次'); location.href='login.php#login'}</script>";
+		if (mysqli_num_rows($result) >= 1) {
+			echo "<script>{window.alert('此信箱已被註冊！請換信箱再試一次'); location.href='register.php'}</script>";
+		} else {
+			$stmt = $conn->prepare("INSERT INTO accounts(accountId, accountName, accountGender, accountBirthday, accountPassword, accountEmail, accountPhoneNumber) VALUES ( REPLACE(UUID(),'-',''), ?,?,?,?,?,?)");
+			$stmt->bind_param("ssssss", $accountName, $accountGender, $accountBirthday, $hashedPassword, $accountEmail, $accountPhoneNumber);
+			if ($stmt->execute()) {
+				echo "<script>{window.alert('註冊成功！'); location.href='login.php'}</script>";
+			} else {
+				echo "Error: " . $stmt->error;
 			}
-    	} 
-		else {
-        	// 若帳號不存在，則顯示錯誤訊息，並轉址至註冊頁面
-        	echo "<script>{window.alert('此電子信箱尚未註冊！請先註冊帳號'); location.href='login.php#register'}</script>";
 		}
-	}
-}
-elseif($_POST["FormType"]== "Register"){
-	//確認前端表單傳送不是空值
-	if (isset($_POST["accountName"])&& isset($_POST["accountGender"])&& isset($_POST["accountBirthday"])&& isset($_POST["accountEmail"]) && isset($_POST["accountPhoneNumber"]) && isset($_POST["accountPassword"])) {
-
-    	//變數設定
-    	$accountName = $_POST["accountName"];
-    	$accountGender = $_POST["accountGender"];
-    	$accountBirthday = $_POST["accountBirthday"];
-    	$accountEmail = $_POST["accountEmail"];
-    	$accountPhoneNumber = $_POST["accountPhoneNumber"];
-    	$accountPassword = $_POST["accountPassword"];
-
-    	//加密密碼
-    	$hashedPassword = password_hash($accountPassword, PASSWORD_DEFAULT);
-
-    	//引入資料庫設定
-    	require_once("php/conn.php");
-
-    	//檢查資料庫內是否已有帳戶
-    	$stmt = $conn->prepare("SELECT accountEmail FROM accounts WHERE accountEmail = ?");
-    	$stmt->bind_param("s", $accountEmail);
-    	$stmt->execute();
-    	$result = $stmt->get_result();
-
-    	if (mysqli_num_rows($result) >= 1) {
-       		echo "<script>{window.alert('此信箱已被註冊！請換信箱再試一次'); location.href='login.php#register'}</script>";
-    	} 
-		else {
-        	$stmt = $conn->prepare("INSERT INTO accounts(accountId, accountName, accountGender, accountBirthday, accountPassword, accountEmail, accountPhoneNumber) VALUES ( REPLACE(UUID(),'-',''), ?,?,?,?,?,?)");
-        	$stmt->bind_param("ssssss", $accountName, $accountGender, $accountBirthday, $hashedPassword, $accountEmail, $accountPhoneNumber);
-        	if ($stmt->execute()) {
-            echo "<script>{window.alert('註冊成功！'); location.href='login.php#login'}</script>";
-        	} 
-			else {
-            echo "Error: " . $stmt->error;
-        	}
-    	}
 	}
 }
 ?>
@@ -116,8 +74,8 @@ elseif($_POST["FormType"]== "Register"){
 </head>
 
 <body>
-	
-	<section class="banner-area relative" >
+
+	<section class="banner-area relative">
 		<div class="overlay overlay-bg"></div>
 		<div class="container">
 			<div class="row fullscreen align-items-center justify-content-between">
@@ -125,53 +83,45 @@ elseif($_POST["FormType"]== "Register"){
 					<h5 class="text-white" style="font-family: Arial Black, sans-serif;">Start Camping</h5>
 					<h1 class="text-white" style="font-size: 40px;">營在起跑點</h1>
 					<p class="text-white" style="font-size: 14px;">
-					邀請你一同進入露營的世界，快速開始一趟冒險旅程，多處地點一覽，不怕踩雷，讓你能夠「營」在起跑點！☺ ☺ ☺
+						邀請你一同進入露營的世界，快速開始一趟冒險旅程，多處地點一覽，不怕踩雷，讓你能夠「營」在起跑點！☺ ☺ ☺
 					</p>
 
 				</div>
 				<div class="col-lg-5 col-md-10 banner-right">
 					<div class="tab-content" id="register">
 						<div class="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="login-tab">
-					
-							<form class="form-wrap" method="post" action="login.php" onsubmit="return checkPassword()" >
-							<h4 style="text-align: center; margin-left: 12px; margin-bottom: 24px; color: ">加入我們！</h4>
+
+							<form class="form-wrap" method="post" action="register.php" onsubmit="return checkPassword()">
+								<h4 style="text-align: center; margin-left: 12px; margin-bottom: 24px; ">加入我們！</h4>
 								<input type="hidden" name="FormType" value="Register">
-								
 
-								<input type="text" class="form-control" style="margin-bottom:10px; border-radius: 25px; background-color:#f1f1f1f1; padding: 0.675rem 1.75rem; name="accountName" placeholder="名稱"
-									onfocus="this.placeholder = ''" onblur="this.placeholder = '名稱'" required>
-								
 
-								<input type="email" class="form-control Email" style="margin-bottom:10px; border-radius: 25px; background-color:#f1f1f1f1; padding: 0.675rem 1.75rem; name="accountEmail" placeholder="電子信箱"
-									onfocus="this.placeholder = ''" onblur="this.placeholder = '電子信箱 '" required>
-								<input type="tel" class="form-control phone" style="margin-bottom:10px; border-radius: 25px; background-color:#f1f1f1f1; padding: 0.675rem 1.75rem;
-									name="accountPhoneNumber" placeholder="電話" onfocus="this.placeholder = ''"
-									onblur="this.placeholder = '電話'" required>
-							<input type="date"  class="form-control " name="accountBirthday" id="birthday" style="width:200px; border-radius: 25px;  line-height: 15px; background-color:#f1f1f1f1; padding: 0.675rem 1.75rem;" required>
-							<div class="form-check form-check-inline" style=" position:relative;  left: 110px; top: -40px;">
-							<input class="form-check-input" type="radio" name="accountGender" value="Female" style="text-align: left;" required>
-							<label class="form-check-label"  for="Female">女性</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-							<input class="form-check-input" type="radio"name="accountGender" value="Male" required>
-							<label  class="form-check-label" for="Male">男性</label></div>
-							<hr style="margin-top :0px; margin-bottom: 2rem;">
-							<input type="password" class="form-control " style="margin-bottom:10px; border-radius: 25px; background-color:#f1f1f1f1; padding: 0.675rem 1.75rem;
-									name="accountPassword" placeholder="密碼" onfocus="this.placeholder = ''"
-									onblur="this.placeholder = '密碼'" id="accountPassword" required>
-								<input type="password" class="form-control " style="margin-bottom:10px; border-radius: 25px; background-color:#f1f1f1f1; padding: 0.675rem 1.75rem;
-									name="checkPassword" placeholder="再次確認密碼 " onfocus="this.placeholder = ''"
-									onblur="this.placeholder = '再次確認密碼'" id="checkPassword">
-									
-									<button class="primary-btn" :hover
-									style=" width:230px;  margin-top: 20px; margin-right: 5px; " id="registerButton">註冊</button>
-									<button class="secondary-btn" :hover style=" width:140px;" ><a :hover href="login.php" >已有帳號？</a></button>
-							
-								</form>
+								<input type="text" class="form-control" style="margin-bottom:10px; border-radius: 25px; background-color:#f1f1f1f1; padding: 0.675rem 1.75rem;" name="accountName" placeholder="名稱" onfocus="this.placeholder = ''" onblur="this.placeholder = '名稱'" required>
+
+
+								<input type="email" class="form-control Email" style="margin-bottom:10px; border-radius: 25px; background-color:#f1f1f1f1; padding: 0.675rem 1.75rem;" name="accountEmail" placeholder="電子信箱" onfocus="this.placeholder = ''" onblur="this.placeholder = '電子信箱 '" required>
+								<input type="tel" class="form-control phone" style="margin-bottom:10px; border-radius: 25px; background-color:#f1f1f1f1; padding: 0.675rem 1.75rem;" name="accountPhoneNumber" placeholder="電話" onfocus="this.placeholder = ''" onblur="this.placeholder = '電話'" required>
+								<input type="date" class="form-control " name="accountBirthday" id="birthday" style="width:200px; border-radius: 25px;  line-height: 15px; background-color:#f1f1f1f1; padding: 0.675rem 1.75rem;" required>
+								<div class="form-check form-check-inline" style=" position:relative;  left: 110px; top: -40px;">
+									<input class="form-check-input" type="radio" name="accountGender" value="Female" style="text-align: left;" required>
+									<label class="form-check-label" for="Female">女性</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+									<input class="form-check-input" type="radio" name="accountGender" value="Male" required>
+									<label class="form-check-label" for="Male">男性</label>
+								</div>
+								<hr style="margin-top :0px; margin-bottom: 2rem;">
+								<input type="password" class="form-control " style="margin-bottom:10px; border-radius: 25px; background-color:#f1f1f1f1; padding: 0.675rem 1.75rem;" name="accountPassword" placeholder="密碼" onfocus="this.placeholder = ''" onblur="this.placeholder = '密碼'" id="accountPassword" required>
+								<input type="password" class="form-control " style="margin-bottom:10px; border-radius: 25px; background-color:#f1f1f1f1; padding: 0.675rem 1.75rem;" name="checkPassword" placeholder="再次確認密碼 " onfocus="this.placeholder = ''" onblur="this.placeholder = '再次確認密碼'" id="checkPassword">
+
+								<button class="primary-btn" :hover style=" width:230px;  margin-top: 20px; margin-right: 5px; " id="registerButton">註冊</button>
+								<a class="secondary-btn" :hover style=" width:140px;" href="login.php">已有帳號？</a>
+
+							</form>
 						</div>
 
 					</div>
 				</div>
 			</div>
-</div>
+		</div>
 		</div>
 
 
@@ -208,7 +158,7 @@ elseif($_POST["FormType"]== "Register"){
 				return true;
 			}
 
-			document.getElementById("registerButton").addEventListener("click", function(event){
+			document.getElementById("registerButton").addEventListener("click", function(event) {
 				if (!checkPassword()) {
 					event.preventDefault();
 				}
