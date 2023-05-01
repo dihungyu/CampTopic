b90f04a295f3c3fd213bf628f13e9e79cdbfe9a5
yizzz002 <104059984+yizzz002@users.noreply.github.com>
2023-05-01 23:52:30 +0800

    跑版

--- property-1.0.0/member-like.php
+++ property-1.0.0/member-like.php
@@ -473,9 +473,11 @@ function format_like_count($count)
                     echo "<div class='col-md-4 sidebar'>
             <aside>
               <div class='aside-body'>
-                <figure class='ads'style='display: flex; justify-content: center;'>
-                    <img src='images/Group 74.png'style='width:50%'>
-                </figure>
+              <figure class='ads'>
+              <a href='#'>
+                <img src='images/Group 74.png'>
+              </a>
+              <figcaption>Advertisement</figcaption>
               </div>
             </aside>
           </div>";
@@ -594,7 +596,7 @@ function format_like_count($count)
                   echo '<div class="card">';
                   echo '<img src="' . $image_src . '" class="card-img-top" alt="...">';
                   echo '<div class="card-body">';
-                  echo '<div class="detail">';
+                  echo '<div class="detail"style="margin-bottom: 0px;">';
                   if ($row["equipmentType"] === "租") {
                     echo '<span class="fa-stack fa-1x" style="margin-right: 5px; ">';
                     echo '<i class="fas fa-circle fa-stack-2x" style="color:#EFE9DA; font-size:24px;"></i>';
@@ -612,11 +614,11 @@ function format_like_count($count)
                     echo '</span>';
                   }
 
-                  echo '<h1><a href="#">' . $row['equipmentName'] . '</a></h1>';
-                  echo '<h5>$' . $row['equipmentPrice'] . '</h5>';
+                  echo '<h5><a href="#">' . $row['equipmentName'] . '</a></h5>';
+                  echo '<h4>$' . $row['equipmentPrice'] . '</h4>';
                   echo '</div>';
                   echo '<p class="card-text">' . $row['equipmentDescription'] . '</p>';
-                  echo "<footer>";
+                  echo "<footer style='padding-top: 10px;'>";
                   echo "<span>";
 
                   // 以下程式碼用於查詢設備相關的標籤
@@ -712,79 +714,47 @@ function format_like_count($count)
 
     <div class="site-footer">
       <div class="container">
-        <div class="row">
-          <div class="col-lg-4">
-            <div class="widget">
-              <h3>Contact</h3>
-              <address>43 Raymouth Rd. Baltemoer, London 3910</address>
-              <ul class="list-unstyled links">
-                <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
-                <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
-                <li>
-                  <a href="mailto:info@mydomain.com">info@mydomain.com</a>
-                </li>
-              </ul>
-            </div>
-            <!-- /.widget -->
+      <div class="row">
+        
+         <!-- /.col-lg-4 -->
+        <div class="col-lg-5">
+          <div class="widget">
+            <h3>聯絡資訊</h3>
+            <address>StartCamping 營在起跑點！</address>
+            <ul class="list-unstyled links">
+              <li><a href="tel://11234567890">0911222345</a></li>
+              <li><a href="tel://11234567890">@startcamping</a></li>
+              <li>
+                <a href="mailto:info@mydomain.com">startcamping@gmail.com</a>
+              </li>
+            </ul>
           </div>
-          <!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <div class="widget">
-              <h3>Sources</h3>
-              <ul class="list-unstyled float-start links">
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Vision</a></li>
-                <li><a href="#">Mission</a></li>
-                <li><a href="#">Terms</a></li>
-                <li><a href="#">Privacy</a></li>
-              </ul>
-              <ul class="list-unstyled float-start links">
-                <li><a href="#">Partners</a></li>
-                <li><a href="#">Business</a></li>
-                <li><a href="#">Careers</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">FAQ</a></li>
-                <li><a href="#">Creative</a></li>
-              </ul>
-            </div>
-            <!-- /.widget -->
-          </div>
-          <!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <div class="widget">
-              <h3>Links</h3>
-              <ul class="list-unstyled links">
-                <li><a href="#">Our Vision</a></li>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Contact us</a></li>
-              </ul>
-
-              <ul class="list-unstyled social">
-                <li>
-                  <a href="#"><span class="icon-instagram"></span></a>
-                </li>
-                <li>
-                  <a href="#"><span class="icon-twitter"></span></a>
-                </li>
-                <li>
-                  <a href="#"><span class="icon-facebook"></span></a>
-                </li>
-                <li>
-                  <a href="#"><span class="icon-linkedin"></span></a>
-                </li>
-                <li>
-                  <a href="#"><span class="icon-pinterest"></span></a>
-                </li>
-                <li>
-                  <a href="#"><span class="icon-dribbble"></span></a>
-                </li>
-              </ul>
-            </div>
-            <!-- /.widget -->
-          </div>
-          <!-- /.col-lg-4 -->
+          <!-- /.widget -->
         </div>
+        <!-- /.col-lg-4 -->
+         <div class="col-lg-5">
+          <div class="widget">
+            <h3>頁面總覽</h3>
+            <ul class="list-unstyled float-start links">
+              <li><a href="#">首頁</a></li>
+              <li><a href="#">找小鹿</a></li>
+              <li><a href="#">鹿的分享</a></li>
+              <li><a href="#">鹿的裝備</a></li>
+              <li><a href="#">廣告方案</a></li>
+            </ul>
+            <ul class="list-unstyled float-start links">
+              <li><a href="#">帳號</a></li>
+              <li><a href="#">會員帳號</a></li>
+              <li><a href="#">我的收藏</a></li>
+            </ul>
+          </div>
+          <!-- /.widget -->
+        </div>
+        <!-- /.col-lg-4 -->
+        <div class="col-lg-2">
+          <!-- /.widget -->
+        </div>
+      </div>
         <!-- /.row -->
 
         <div class="row mt-5">
