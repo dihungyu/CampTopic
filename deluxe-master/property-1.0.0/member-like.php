4a529dfd771a5902a89e5bef0c7409e71598efa2
yizzz002 <104059984+yizzz002@users.noreply.github.com>
2023-05-04 00:24:06 +0800

    issue8完成

--- deluxe-master/property-1.0.0/member-like.php
+++ deluxe-master/property-1.0.0/member-like.php
@@ -94,12 +94,12 @@ function format_like_count($count)
         <ul class="navbar-nav ml-auto">
           <li class="nav-item "><a href="index.html" class="nav-link">首頁</a></li>
           <li class="nav-item"><a href="rooms.html" class="nav-link">找小鹿</a></li>
-          <li class="nav-item"><a href="restaurant.html" class="nav-link">鹿的分享</a></li>
-          <li class="nav-item"><a href="about.html" class="nav-link">鹿的裝備</a></li>
+          <li class="nav-item"><a href="../all-article.html" class="nav-link">鹿的分享</a></li>
+          <li class="nav-item"><a href="../equipment.html" class="nav-link">鹿的裝備</a></li>
           <li class="nav-item"><a href="blog.html" class="nav-link">廣告方案</a></li>
 
           <li class="nav-item dropdown active">
-            <a class="nav-link dropdown-toggle" href="member.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="member.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               帳號
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
